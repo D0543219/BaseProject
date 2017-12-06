4687d517fbe304916460370a2c0440733ba3ef16
D0543219 <delete135430@gmail.com>
2017-12-06 12:11:20 +0800

    12

---
+++ src/main/java/com/github/D0543219/D0543219.java
@@ -0,0 +1,10 @@
+package com.github.D0543219;
+public class D0543219 {
+    public int test(){
+        int i = 3;
+        if(i % 2 == 0)
+            return 1;
+        else
+            return 2;
+    }
+}
---
+++ src/test/java/com/github/D0543219/D0543219Test.java
@@ -0,0 +1,14 @@
+package com.github.D0543219;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class D0543219Test {
+    @Test
+    public void test1() throws Exception {
+        D0543219 a = new D0543219();
+        System.out.print(a.test());
+    }
+
+}
